04db919a1230a68fd6e9b0c5f69128809ffccf14
vincent <vincentvo@github.com>
2013-12-04 01:03:32 -0800

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TwitterScaffolding::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
